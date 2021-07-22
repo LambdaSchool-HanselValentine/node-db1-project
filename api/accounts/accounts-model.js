9c5295b4ff06b5e4abe79d64918f76fc82570a3d
HanselV <hanselviva@gmail.com>
2021-07-21 21:30:14 -0700

    model done

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,42 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+	// DO YOUR MAGIC
+	return db("accounts");
+};
 
-const getById = id => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+	// DO YOUR MAGIC
+	const account = db.first("*").from("accounts").where({ id });
+	return account;
+};
 
-const create = account => {
-  // DO YOUR MAGIC
-}
+const create = async (account) => {
+	// DO YOUR MAGIC
+	const newAcc = await db("accounts").insert(account);
+	return newAcc;
+};
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
+const updateById = async (id, account) => {
+	// DO YOUR MAGIC
+	const updatedAccount = await db
+		.update(account)
+		.from("accounts")
+		.where({ id });
+	console.log(updatedAccount, "updated acc");
+	return updatedAccount;
+};
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+const deleteById = async (id) => {
+	// DO YOUR MAGIC
+	const deleted = await db("accounts").del().where({ id });
+	return deleted;
+};
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
-}
+	getAll,
+	getById,
+	create,
+	updateById,
+	deleteById,
+};
